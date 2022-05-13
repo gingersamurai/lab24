e0ccb7013d4350189ed3180179176ecd88bb8e60
Nazim Malyshev <nazimmalyshev47@gmail.com>
2022-05-13 15:39:44 +0300

    1.5 commit

---
+++ token.h
@@ -0,0 +1,14 @@
+#ifndef transform_h
+#define transform_h
+
+typedef struct{
+    char type; // o () i f v F
+    int val_int;
+    float val_float;
+    char op;
+    char var_name;
+} token;
+
+
+
+#endif
